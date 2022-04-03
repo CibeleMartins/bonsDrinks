c14b829727744d728a5c39ae0b3c628aace1353b
Cibele <cibelemartins24@hotmail.com>
2022-04-03 17:27:28 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {Route, Router, Switch, Link} from 'react-router-dom';
+import Drinks from './components/pages/Drinks';
+import DrinksFunc from './components/pages/DrinksFunc';
+import Sobre from './components/pages/Sobre';
+import NossoTime from './components/pages/NossoTime';
+import Contato from './components/pages/Contato';
+
+import Container from './components/pages/layout/Container';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+
+    <div>
+    <Link to='/drinks'>Drinks</Link>
+    <Link to='/drinksFunc'>DrinksFunc</Link>
+    <Link to='/sobre'>Sobre nós</Link>
+    <Link to='/NossoTime'>Nosso Time</Link>
+    <Link to='/contato'>Contato</Link>
     </div>
-  );
+
+    <Switch>
+
+      {/* Em cada Route tem um componente/pagina, no atributo path colocamos o nome do caminho da route */}
+
+      <Container>
+
+        <Route exact path='/drinks'>
+          {/* "exact" para executar a home quando a rotta for exatamente a barra */}
+          <Drinks/>
+
+        </Route>
+
+        <Route exact path='/drinksFunc'>
+          <DrinksFunc/>
+        </Route>
+
+        <Route exact path='/sobre'>
+          <Sobre/>
+        </Route>
+
+        <Route exact path='/NossoTime'>
+          <NossoTime/>
+        </Route>
+
+        <Route exact path='/contato'>
+          <Contato/>
+        </Route>
+
+      </Container>
+    </Switch>
+
+    <p>Footer</p>
+  </Router>
+
+  )
 }
 
 export default App;
+
+
+// Depois disso, é criar esses componentes, as páginas para estes componentes
+//  primeiro vai em src e cria a pasta components
+// cria a pasta pages
+// cria um arquivo JS para cada componente
---
+++ src/components/pages/NossoTime.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NossoTime () {
+
+  return <h1>Nosso Time</h1>
+}
+
+
+export default NossoTime;
